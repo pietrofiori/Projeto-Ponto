c73af9cd942b0228717e3158a217c2c3d0c872d5
Pietro Fiori <78088814+pietrofiori@users.noreply.github.com>
2024-04-28 19:53:04 -0300

    Update Final do Ponto

---
+++ Commands.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE registro_ponto;
+
+\c registro_ponto;
+
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    cpf VARCHAR(14) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL
+    data_nascimento DATE NOT NULL,
+);
+
+CREATE TABLE registro_pontos (
+    id SERIAL PRIMARY KEY,
+    usuario_id INTEGER REFERENCES usuarios(id),
+    data_entrada TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_intervalo TIMESTAMP,
+    data_fim_intervalo TIMESTAMP,
+    data_saida TIMESTAMP
+);
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
 const pool = require('../connectionDB');
@@ -11,10 +11,16 @@ router.post('/login', async (req, res) => {
     if (user.rows.length === 0) {
       return res.status(401).json({ message: 'Credenciais inválidas' });
     }
+
+    // geração do JWT
+    const token = jwt.sign({ id: user.rows[0].id }, 'akira', { expiresIn: '1h' });
+    res.status(200).json({ token, user: user.rows[0] });
+    console.log("TOKEN " + token);
   } catch (error) {
     console.error('Erro ao fazer login:', error.message);
     res.status(500).json({ message: 'Erro interno do servidor' });
   }
 });
+
 
 module.exports = router;
