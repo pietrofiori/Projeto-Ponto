7a6f6efb8ccbf5453929ad6af17f8851c463e0d9
Pietro Fiori <78088814+pietrofiori@users.noreply.github.com>
2024-04-29 20:30:29 -0300

    Update Commands.sql

--- Commands.sql
+++ Commands.sql
@@ -7,8 +7,8 @@ CREATE TABLE usuarios (
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     cpf VARCHAR(14) UNIQUE NOT NULL,
-    senha VARCHAR(255) NOT NULL
-    data_nascimento DATE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    data_nascimento DATE NOT NULL
 );
 
 CREATE TABLE registro_pontos (
